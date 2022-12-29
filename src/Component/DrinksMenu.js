abe0e84d2b1de8c9664e907d83efebfb35859fe3
Thiratikan <thiratikan@gmail.com>
2022-12-28 23:40:26 -0500

    Drinks done

--- src/Component/DrinksMenu.js
+++ src/Component/DrinksMenu.js
@@ -1,22 +1,141 @@
 import React from "react";
-import { DRINKS, Title } from "../styles/styleDrinks";
+import { DRINKS, Title, item, text } from "../styles/styleDrinks";
 import Drinks from "./data/Drinks.json";
 
 function DrinksMenu() {
   return (
     <>
       <DRINKS>
-        <Title>Let's Drink</Title>
+        <Title id="drinks">Let's Drink</Title>
         <section>FUN MIXED</section>
-        {Drinks.drinks.map((item) => {
-          const { title, price } = item;
-          return (
-            <div>
-              <h1>{title}</h1>
-              <h2>{price}</h2>
-            </div>
-          );
-        })}
+        <item>
+          {Drinks.drinks
+            .filter((i) => i.category === "FUN MIXED")
+            .map((item) => {
+              const { title, price, id } = item;
+              return (
+                <div key={id}>
+                  <text>
+                    <h1>{title}</h1>
+                    <h1 className="price"> {price}</h1>
+                  </text>
+                </div>
+              );
+            })}
+        </item>
+        <img src="/images/martini.png" alt="" />
+        <section>MARTINI</section>
+        <item>
+          {Drinks.drinks
+            .filter((i) => i.category === "MARTINI")
+            .map((item) => {
+              const { title, price, id } = item;
+              return (
+                <div key={id}>
+                  <text>
+                    <h1>{title}</h1>
+                    <h1 className="price"> {price}</h1>
+                  </text>
+                </div>
+              );
+            })}
+        </item>
+        <section>SAKE</section>
+        <item>
+          {Drinks.drinks
+            .filter((i) => i.category === "SAKE")
+            .map((item) => {
+              const { title, price, id } = item;
+              return (
+                <div key={id}>
+                  <text>
+                    <h1>{title}</h1>
+                    <h1 className="price"> {price}</h1>
+                  </text>
+                </div>
+              );
+            })}
+        </item>
+        <section>BEER</section>
+        <item>
+          {Drinks.drinks
+            .filter((i) => i.category === "BEER")
+            .map((item) => {
+              const { title, price, id } = item;
+              return (
+                <div key={id}>
+                  <text>
+                    <h1>{title}</h1>
+                    <h1 className="price"> {price}</h1>
+                  </text>
+                </div>
+              );
+            })}
+        </item>
+        <section>WHITE WINE</section>
+        <item>
+          {Drinks.drinks
+            .filter((i) => i.category === "WHITE WINE")
+            .map((item) => {
+              const { title, price, id } = item;
+              return (
+                <div key={id}>
+                  <text>
+                    <h1>{title}</h1>
+                    <h1 className="price"> {price}</h1>
+                  </text>
+                </div>
+              );
+            })}
+        </item>
+        <section>ROŚE</section>
+        <item>
+          {Drinks.drinks
+            .filter((i) => i.category === "ROŚE")
+            .map((item) => {
+              const { title, price, id } = item;
+              return (
+                <div key={id}>
+                  <text>
+                    <h1>{title}</h1>
+                    <h1 className="price"> {price}</h1>
+                  </text>
+                </div>
+              );
+            })}
+        </item>
+        <section>RED WINE</section>
+        <item>
+          {Drinks.drinks
+            .filter((i) => i.category === "RED WINE")
+            .map((item) => {
+              const { title, price, id } = item;
+              return (
+                <div key={id}>
+                  <text>
+                    <h1>{title}</h1>
+                    <h1 className="price"> {price}</h1>
+                  </text>
+                </div>
+              );
+            })}
+        </item>
+        <section>NON ALCOHOLIC</section>
+        <item>
+          {Drinks.drinks
+            .filter((i) => i.category === "NON ALCOHOLIC")
+            .map((item) => {
+              const { title, price, id } = item;
+              return (
+                <div key={id}>
+                  <text>
+                    <h1>{title}</h1>
+                    <h1 className="price"> {price}</h1>
+                  </text>
+                </div>
+              );
+            })}
+        </item>
       </DRINKS>
     </>
   );
--- src/styles/styleDrinks.js
+++ src/styles/styleDrinks.js
@@ -1,10 +1,21 @@
 import styled from "styled-components";
 
+export const Title = styled.div`
+  font-family: "Kaushan Script", cursive;
+  font-size: 40px;
+  margin-left: 20px;
+`;
+
+export const item = styled.div`
+  background-image: none;
+`;
+
 export const DRINKS = styled.div`
   padding: 20px;
   font-family: "Roboto", sans-serif;
   font-size: 6px;
   font-weight: lighter;
+
   section {
     font-weight: bold;
     font-size: 15px;
@@ -13,11 +24,25 @@ export const DRINKS = styled.div`
   div {
     display: flex;
     justify-content: space-between;
+    position: relative;
+    margin-bottom: 5px;
   }
-`;
+  h1 {
+    background-image: none;
+    display: inline;
+  }
+  img {
+    height: 200px;
+    weight: 150px;
+    position: absolute;
+    z-index: -1;
+    left: 160px;
+    bottom: -70px;
+    background-image: none;
+  }
 
-export const Title = styled.div`
-  font-family: "Kaushan Script", cursive;
-  font-size: 40px;
-  margin-left: 20px;
+  .price {
+    position: absolute;
+    left: 299px;
+  }
 `;
